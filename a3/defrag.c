abbb7c4012df79b94486ef3a6fff8e0ad469c3fa
Brenden Rasmussen <54872409+brasmussen117@users.noreply.github.com>
2021-04-29 15:45:41 -0400

    working

--- a3/defrag.c
+++ a3/defrag.c
@@ -2,7 +2,7 @@
 
 /* function prototypes --------------------------------------------- */
 void *searchdir(void *);
-void makeindex(char *, DIR *);
+void makeindex(struct dirent *, const char *);
 
 int main(int argc, char const *argv[])
 {
@@ -38,14 +38,15 @@ int main(int argc, char const *argv[])
     }
 
     /* prep -------------------------------------------------------- */
-    mp3_entries = NULL;
-    mp3_length = malloc(sizeof(int));
-    *mp3_length = 0;
-    
-    struct dirent *dir_entry = NULL;
-    DIR *next_dir = NULL;
+    // dirarr_t *dir_arr = malloc(sizeof(dirarr_t));
+    // dir_arr->arr = NULL;
+    // dir_arr->length = malloc(sizeof(int));
+    // dir_arr->length = 0;
 
-    // int err;
+    struct dirent *dir_entry = NULL;
+    dir_t *next_dir = malloc(sizeof(dir_t));
+
+    int err; // TODO: finish this
 	
 	/* top loop ---------------------------------------------------- */
 
@@ -59,19 +60,19 @@ int main(int argc, char const *argv[])
 
 		if (dir_entry->d_type == DT_DIR) // check if dir_entry entry is subdir
 		{
-            if (!(next_dir = opendir(dir_entry->d_name))) // check if next_dir can be made
+            next_dir->dir_entry = dir_entry;
+            next_dir->path = catpath(dir_entry->d_name, top_name);
+            fprintf(stdout, "main: opened subdir (\"%s\")", dir_entry->d_name);
+            // TODO: handle it
+            err = pthread_create(&tids[thread_count++], NULL, searchdir, next_dir);
+            if (err != 0) 
             {
-                perror("ERROR: main: could not open next_dir");
+                perror("ERROR: main: could not create thread");
                 exit(EXIT_FAILURE);
             }
-
-            // TODO: handle it
-            // err = pthread_create((pthread_t)dir_entry->d_ino, NULL, searchdir, next_dir);
 		} else if (is_mp3(dir_entry->d_name)) // check if file ext is mp3
 		{ 
-			makeindex(dir_entry->d_name);
-			// handle the file
-
+			makeindex(dir_entry, top_name);
 		} // else file was other type not used here, skip it
 	}
 
@@ -85,52 +86,76 @@ int main(int argc, char const *argv[])
 }
 
 /* thread func to search a dir for mp3 files */
-// void *searchdir(void *arg){
-//     DIR *current = (DIR *)arg;
+void *searchdir(void *arg){
+    // DIR *current = (DIR *)arg;
 
+    fprintf(stdout, "searchdir: ");
 
-//     /* 
-//     loop through each entry
-//         if entry is subdir, call self
-//         if entry is mp3, call makeindex
-//      */
-// }
+    /* 
+    loop through each entry
+        if entry is subdir, call self
+        if entry is mp3, call makeindex
+     */
+    return arg;
+}
 
-/* make an index entry in mp3_entries from filepath/filename */
-void makeindex(char *filename){
-    
+/* make an index entry in mp3_entries from filepath/filename 
+    TODO: fix description
+*/
+void makeindex(struct dirent *entry, const char *filepath){
+    /* get int value of filename ----------------------------------- */
+    int file_int = get_int(entry->d_name);
 
-
-//     strcpy(buf, "./");
-//     char *filepath = realpath(strcat(buf, filename), NULL);
-
-//     if (filepath == NULL)
-//     {
-//         perror("ERROR: makeindex: failed to get path");
-//         exit(EXIT_FAILURE);
-//     }
-        
-    char *file_str = strdup(filename); // dup filename for strsep
-    char *free_file_str = file_str;
-
-    int file_int = atoi(strsep(&file_str, ".")); // sep and get int value
-
-    if (*mp3_length < (file_int + 1)) // check that mp3_entries is large enough
+    /* check mp3_index size, realloc as needed --------------------- */
+    if (mp3_index_length < (file_int + 1)) // check that mp3_index is large enough
     {
-        mp3_entries = realloc(mp3_entries, (sizeof(char *)*(file_int + 1))); // realloc big enoug
-        if (mp3_entries == NULL) // check that realloc was successful
+        mp3_index = realloc(mp3_index, (sizeof(void *)*(file_int + 1))); // realloc big enoug
+        if (mp3_index == NULL) // check that realloc was successful
         {
-            perror("ERROR: makeindex: failed to realloc mp3_entries");
+            perror("ERROR: makeindex: failed to realloc mp3_index");
             exit(EXIT_FAILURE);
         }
     }
-    
-//     // strcpy(buf, filepath); // copy the filepath into buf
-//     // strcat(buf, "/"); // cat a slash between path and name
-//     // strcat(buf, filename); // cat the filename
 
-    mp3_entries[file_int] = strdup(filepath); // dup entry into array
-    *mp3_length = *mp3_length + 1; // inc mp3_length
+    /* put filename to index --------------------------------------- */
+    *mp3_index[file_int]->filename = file_int;
+
+    /* get full path name and put to index ------------------------- */
+    mp3_index[file_int]->fullpath = catpath(entry->d_name, filepath);
+
+    /* open file contents ------------------------------------------ */
+    FILE *contents = fopen(mp3_index[file_int]->fullpath, O_RDONLY);
+
+    if (contents == NULL) // check that fopen was sucessful
+    {
+        if (errno == ENOENT)
+        {
+            fprintf(stderr, "ERROR: makeindex: cannot open(\"%s\"): No such file or directory\n", entry->d_name);
+        }
+        else
+        {
+            perror("ERROR: makeindex: cannot open contents");
+        }
+        exit(EXIT_FAILURE);
+    }
+
+    /* malloc and read contents into mp3_index ---------------------- */
+
+    mp3_index[file_int]->data = malloc(entry->d_reclen); // malloc space for newest entry
+
+    if (fread(mp3_index[file_int]->data, 1, entry->d_reclen, contents) 
+        != entry->d_reclen)
+    {
+        perror("ERROR: makeindex: cannot read file contents");
+        exit(EXIT_FAILURE);
+    }
+
+    mp3_index_length++; // inc mp3_index_length
     
-    free(free_file_str); // free memory
+    /* close file -------------------------------------------------- */
+    if (fclose(contents) != 0) // check file close success
+    {
+        fprintf(stderr, "parser::main: file not successfully closed: (\"%s\")\n", entry->d_name);
+        exit(EXIT_FAILURE);
+    }
 }
--- a3/defrag_header.h
+++ a3/defrag_header.h
@@ -12,21 +12,37 @@
 /* #endregion includes */
 
 /* #region global variables/const ---------------------------------- */
+#define MAXTHREADS 100
+
+pthread_t tids[MAXTHREADS];
+
+int thread_count = 0;
+
 pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 
-/* mp3 array */
-char **mp3_entries;
-int *mp3_length;
-
-char buf[MAXNAMLEN];
 /* #endregion variables/const */
 
 /* #region structs ------------------------------------------------- */
-typedef struct index
-{
-	char **entries;
+
+typedef struct mp3info {
+	int *filename;
+	char *fullpath;
+	void *data;
+} mp3info_t;
+
+mp3info_t **mp3_index;
+int mp3_index_length = 0;
+
+typedef struct dir {
+	struct dirent *dir_entry;
+	char *path;
+} dir_t;
+
+typedef struct dirarr {
+	dir_t **arr;
 	int *length;
-} index_t;
+} dirarr_t;
+
 /* #endregion structs */
 
 /* #region functions ----------------------------------------------- */
@@ -51,4 +67,28 @@ bool is_mp3(const char *filename)
 {
 	return ((strcmp(get_filename_ext(filename), "mp3") == 0) ? true : false);
 }
+
+/* return int value of a filename */
+int get_int(char *filename){
+    char *file_str = strdup(filename); // dup filename for strsep
+    char *free_file_str = file_str; // get pointer to beginning of file_str
+
+    int file_int = atoi(strsep(&file_str, ".")); // sep and get int value
+
+	free(free_file_str); // free memory
+
+	return file_int;
+}
+
+/* concatenate filepath/filename */
+char *catpath(char *filename, const char *filepath){
+    char buf[MAXNAMLEN];
+
+	strcpy(buf, filepath); // copy the filepath into buf 
+    strcat(buf, "/"); // cat a slash between path and name
+    strcat(buf, filename); // cat the filename
+
+	return strdup(buf);
+}
+
 /* #endregion functions */
